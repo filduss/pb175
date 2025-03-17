bedf30067b34f5e33b43c64c191c9cbc52cf4391
filduss <filip.kolar92@seznam.cz>
2025-03-17 23:43:55 +0100

    feat: add listbox pacients

--- Hospital_IS/Hospital_IS/FormDoctor.Designer.cs
+++ Hospital_IS/Hospital_IS/FormDoctor.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxPacients = new System.Windows.Forms.ListBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxPacients
+            // 
+            this.listBoxPacients.FormattingEnabled = true;
+            this.listBoxPacients.Location = new System.Drawing.Point(12, 12);
+            this.listBoxPacients.Name = "listBoxPacients";
+            this.listBoxPacients.Size = new System.Drawing.Size(158, 394);
+            this.listBoxPacients.TabIndex = 0;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(13, 413);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(157, 23);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Smazat pacienta";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // FormDoctor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.listBoxPacients);
+            this.Name = "FormDoctor";
             this.Text = "FormDoctor";
+            this.Load += new System.EventHandler(this.FormDoctor_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxPacients;
+        private System.Windows.Forms.Button buttonDelete;
     }
 }
--- Hospital_IS/Hospital_IS/FormDoctor.cs
+++ Hospital_IS/Hospital_IS/FormDoctor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,11 +13,67 @@ namespace WindowsFormsApp1
 {
     public partial class FormDoctor : Form
     {
-        string connectionPassword = null;
+        private string connectionPassword = null;
+        private List<PacientBasicInfo> Pacients = new List<PacientBasicInfo>();
+        class PacientBasicInfo
+        {
+            private int id;
+            private string name;
+            private string email;
+
+            public PacientBasicInfo(int id, string name, string email)
+            {
+                this.id = id;
+                this.name = name;
+                this.email = email;
+            }
+
+            public int Id { get { return id; } }
+            public string Name { get { return name; } }
+            public string Email { get { return email; } }
+        }
         public FormDoctor(string connectionPassword)
         {
             this.connectionPassword = connectionPassword;
             InitializeComponent();
+        }
+
+        private void FormDoctor_Load(object sender, EventArgs e)
+        {
+            string connectionString = string.Format("Server=tcp:pb175database.database.windows.net,1433;Initial Catalog=pb175database;Persist Security Info=False;User ID=pb175admin;Password= {0};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;", connectionPassword);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                
+                string query = $"SELECT Id, UserName, Email FROM dbo.pacients";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            PacientBasicInfo pacient = new PacientBasicInfo(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+                            Pacients.Add(pacient);
+                        }
+                    }
+
+                }
+                foreach (var pacient in Pacients)
+                {
+                    listBoxPacients.Items.Add(pacient.Id + "\t" + pacient.Name);
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxPacients.SelectedIndex == -1)
+            {
+                MessageBox.Show("Vyberte pacienta, kterého chcete smazat", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            MessageBox.Show(Pacients[listBoxPacients.SelectedIndex].Name + "\n"+ Pacients[listBoxPacients.SelectedIndex].Id + "\n" + Pacients[listBoxPacients.SelectedIndex].Email);
         }
     }
 }
