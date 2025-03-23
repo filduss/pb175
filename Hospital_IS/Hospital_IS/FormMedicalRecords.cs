043d23e27ffe327618e36f7ea36a3c67cf9116b2
Katarína Balušeskulová <110058670+CatherineBalu@users.noreply.github.com>
2025-03-23 21:38:40 +0100

    add: medical record form and patient form

--- Hospital_IS/Hospital_IS/FormMedicalRecords.cs
+++ Hospital_IS/Hospital_IS/FormMedicalRecords.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,62 @@ namespace WindowsFormsApp1
             InitializeComponent();
             this.patientId = patientId;
             this.connectionPassword = connectionPassword;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<MedicalRecordBasicInfo> records = new List<MedicalRecordBasicInfo>();
+
+        private void FormMedicalRecords_Load(object sender, EventArgs e)
+        {
+            string connectionString = string.Format(
+        "Server=tcp:pb175database.database.windows.net,1433;Initial Catalog=pb175database;Persist Security Info=False;User ID=pb175admin;Password={0};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;",
+        connectionPassword);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = @"SELECT mr.Id, d.UserName AS DoctorName, mr.RecordDate, mr.RecordText
+                                FROM dbo.health_records mr
+                                JOIN dbo.doctors d ON mr.DoctorId = d.Id
+                                WHERE mr.PatientId = @PatientId
+                                ORDER BY mr.RecordDate DESC";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@PatientId", patientId);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string doctor = reader.GetString(1);
+                            DateTime date = reader.GetDateTime(2);
+                            string text = reader.GetString(3);
+
+                            string summary = $"{date.ToShortDateString()} – {doctor}";
+                            MedicalRecordBasicInfo record = new MedicalRecordBasicInfo(id, summary, text);
+                            records.Add(record);
+                        }
+                    }
+                }
+            }
+
+            listBoxRecords.DataSource = records;
+            listBoxRecords.DisplayMember = "Summary";
+        }
+
+        private void listBoxRecords_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxRecords.SelectedItem is MedicalRecordBasicInfo selected)
+            {
+                textBoxDetail.Text = selected.FullText;
+            }
         }
     }
 }
---
+++ Hospital_IS/Hospital_IS/MedicalRecordBasicInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class MedicalRecordBasicInfo
+    {
+        public int Id { get; }
+        public string Summary { get; }
+        public string FullText { get; }
+
+        public MedicalRecordBasicInfo(int id, string summary, string fullText)
+        {
+            Id = id;
+            Summary = summary;
+            FullText = fullText;
+        }
+
+        public override string ToString()
+        {
+            return Summary;
+        }
+    }
+}
