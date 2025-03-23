d13289456adb1ed61b0a18db88182536b2d7b228
Katarína Balušeskulová <110058670+CatherineBalu@users.noreply.github.com>
2025-03-23 21:55:36 +0100

    modified: signing form to improve readability

--- Hospital_IS/Hospital_IS/Form1.Designer.cs
+++ Hospital_IS/Hospital_IS/Form1.Designer.cs
@@ -40,10 +40,11 @@
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(93, 230);
-            this.buttonLogin.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.Location = new System.Drawing.Point(124, 295);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(197, 60);
+            this.buttonLogin.Size = new System.Drawing.Size(263, 74);
             this.buttonLogin.TabIndex = 0;
             this.buttonLogin.Text = "Potvrdit";
             this.buttonLogin.UseVisualStyleBackColor = true;
@@ -51,68 +52,78 @@
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxEmail.Location = new System.Drawing.Point(93, 125);
-            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxEmail.Location = new System.Drawing.Point(124, 154);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(197, 26);
+            this.textBoxEmail.Size = new System.Drawing.Size(263, 28);
             this.textBoxEmail.TabIndex = 1;
             // 
             // labelEmail
             // 
             this.labelEmail.AutoSize = true;
-            this.labelEmail.Location = new System.Drawing.Point(93, 107);
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEmail.Location = new System.Drawing.Point(123, 123);
+            this.labelEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(35, 13);
+            this.labelEmail.Size = new System.Drawing.Size(56, 20);
             this.labelEmail.TabIndex = 2;
             this.labelEmail.Text = "Email:";
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(93, 166);
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(123, 204);
+            this.labelPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(37, 13);
+            this.labelPassword.Size = new System.Drawing.Size(58, 20);
             this.labelPassword.TabIndex = 4;
             this.labelPassword.Text = "Heslo:";
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBoxPassword.Location = new System.Drawing.Point(93, 184);
-            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPassword.Location = new System.Drawing.Point(124, 235);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
-            this.textBoxPassword.Size = new System.Drawing.Size(197, 30);
+            this.textBoxPassword.Size = new System.Drawing.Size(263, 30);
             this.textBoxPassword.TabIndex = 3;
             // 
             // comboBoxRole
             // 
+            this.comboBoxRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxRole.FormattingEnabled = true;
             this.comboBoxRole.Items.AddRange(new object[] {
             "DOKTOR",
             "PACIENT"});
-            this.comboBoxRole.Location = new System.Drawing.Point(93, 73);
+            this.comboBoxRole.Location = new System.Drawing.Point(124, 81);
+            this.comboBoxRole.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxRole.Name = "comboBoxRole";
-            this.comboBoxRole.Size = new System.Drawing.Size(197, 21);
+            this.comboBoxRole.Size = new System.Drawing.Size(263, 28);
             this.comboBoxRole.TabIndex = 5;
             // 
             // labelRole
             // 
             this.labelRole.AutoSize = true;
-            this.labelRole.Location = new System.Drawing.Point(90, 57);
+            this.labelRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRole.Location = new System.Drawing.Point(123, 48);
+            this.labelRole.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelRole.Name = "labelRole";
-            this.labelRole.Size = new System.Drawing.Size(73, 13);
+            this.labelRole.Size = new System.Drawing.Size(112, 20);
             this.labelRole.TabIndex = 6;
             this.labelRole.Text = "Typ uživatele:";
             // 
             // labelCreateAccount
             // 
             this.labelCreateAccount.AutoSize = true;
-            this.labelCreateAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelCreateAccount.Location = new System.Drawing.Point(179, 292);
+            this.labelCreateAccount.BackColor = System.Drawing.SystemColors.Window;
+            this.labelCreateAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelCreateAccount.Location = new System.Drawing.Point(190, 382);
+            this.labelCreateAccount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCreateAccount.Name = "labelCreateAccount";
-            this.labelCreateAccount.Size = new System.Drawing.Size(111, 13);
+            this.labelCreateAccount.Size = new System.Drawing.Size(172, 20);
             this.labelCreateAccount.TabIndex = 7;
             this.labelCreateAccount.Text = "Nemám účet pacienta";
             this.labelCreateAccount.Click += new System.EventHandler(this.labelCreateAccount_Click);
@@ -121,9 +132,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 361);
+            this.ClientSize = new System.Drawing.Size(525, 444);
             this.Controls.Add(this.labelCreateAccount);
             this.Controls.Add(this.labelRole);
             this.Controls.Add(this.comboBoxRole);
@@ -132,7 +143,7 @@
             this.Controls.Add(this.labelEmail);
             this.Controls.Add(this.textBoxEmail);
             this.Controls.Add(this.buttonLogin);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Shown += new System.EventHandler(this.Form1_Shown);
